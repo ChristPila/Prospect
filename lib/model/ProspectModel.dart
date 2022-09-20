f86f21a2074980726f6eda248cacf325d1edddb0
victorine Kaleko <victorinekaleko@gmail.com>
2022-09-20 10:36:42 +0100

    Le modele du prospect

--- lib/model/ProspectModel.dart
+++ lib/model/ProspectModel.dart
@@ -11,83 +11,239 @@ String prospectModelToJson(ProspectModel data) => json.encode(data.toJson());
 class ProspectModel {
   ProspectModel({
     this.id,
-    this.location,
-    this.agentId,
-    this.province,
-    this.ville,
-    this.zone,
+    this.latitude,
+    this.longitude,
+    this.agent,
     this.commune,
     this.companyName,
-    this.companyAdress,
+    this.companyAddress,
     this.companyType,
     this.companyPhone,
-    this.offer,
-    this.visuel1,
-    this.visuel2,
-    this.document1,
-    this.document2,
-    this.signature,
+    this.offre,
     this.state,
+    this.piecesjointes,
+    this.remoteId,
   });
 
   int? id;
-  String? location;
-  int? agentId;
-  int? province;
-  int? ville;
-  String? zone;
-  int? commune;
-  int? companyName;
-  String? companyAdress;
-  String? companyType;
-  int? companyPhone;
-  int? offer;
-  String? visuel1;
-  String? visuel2;
-  String? document1;
-  String? document2;
-  String? signature;
+  String? latitude;
+  String? longitude;
+  Agent? agent;
+  Commune? commune;
+  String? companyName;
+  String? companyAddress;
+  CompanyType? companyType;
+  String? companyPhone;
+  Offre? offre;
   String? state;
+  Piecesjointes? piecesjointes;
+  String? remoteId;
 
   factory ProspectModel.fromJson(Map<String, dynamic> json) => ProspectModel(
     id: json["id"] == null ? null : json["id"],
-    location: json["location"] == null ? null : json["location"],
-    agentId: json["agent_id"] == null ? null : json["agent_id"],
-    province: json["province"] == null ? null : json["province"],
-    ville: json["ville"] == null ? null : json["ville"],
-    zone: json["zone"] == null ? null : json["zone"],
-    commune: json["commune"] == null ? null : json["commune"],
+    latitude: json["latitude"] == null ? null : json["latitude"],
+    longitude: json["longitude"] == null ? null : json["longitude"],
+    agent: json["agent"] == null ? null : Agent.fromJson(json["agent"]),
+    commune: json["commune"] == null ? null : Commune.fromJson(json["commune"]),
     companyName: json["company_name"] == null ? null : json["company_name"],
-    companyAdress: json["company_adress"] == null ? null : json["company_adress"],
-    companyType: json["company_type"] == null ? null : json["company_type"],
+    companyAddress: json["company_address"] == null ? null : json["company_address"],
+    companyType: json["company_type"] == null ? null : CompanyType.fromJson(json["company_type"]),
     companyPhone: json["company_phone"] == null ? null : json["company_phone"],
-    offer: json["offer"] == null ? null : json["offer"],
-    visuel1: json["visuel1"] == null ? null : json["visuel1"],
-    visuel2: json["visuel2"] == null ? null : json["visuel2"],
-    document1: json["document1"] == null ? null : json["document1"],
-    document2: json["document"] == null ? null : json["document"],
-    signature: json["signature"] == null ? null : json["signature"],
+    offre: json["offre"] == null ? null : Offre.fromJson(json["offre"]),
     state: json["state"] == null ? null : json["state"],
+    piecesjointes: json["piecesjointes"] == null ? null : Piecesjointes.fromJson(json["piecesjointes"]),
+    remoteId: json["remote_id"] == null ? null : json["remote_id"],
   );
 
   Map<String, dynamic> toJson() => {
     "id": id == null ? null : id,
-    "location": location == null ? null : location,
-    "agent_id": agentId == null ? null : agentId,
-    "province": province == null ? null : province,
-    "ville": ville == null ? null : ville,
-    "zone": zone == null ? null : zone,
-    "commune": commune == null ? null : commune,
+    "latitude": latitude == null ? null : latitude,
+    "longitude": longitude == null ? null : longitude,
+    "agent": agent == null ? null : agent!.toJson(),
+    "commune": commune == null ? null : commune!.toJson(),
     "company_name": companyName == null ? null : companyName,
-    "company_adress": companyAdress == null ? null : companyAdress,
-    "company_type": companyType == null ? null : companyType,
+    "company_address": companyAddress == null ? null : companyAddress,
+    "company_type": companyType == null ? null : companyType!.toJson(),
     "company_phone": companyPhone == null ? null : companyPhone,
-    "offer": offer == null ? null : offer,
-    "visuel1": visuel1 == null ? null : visuel1,
-    "visuel2": visuel2 == null ? null : visuel2,
-    "document1": document1 == null ? null : document1,
-    "document": document2 == null ? null : document2,
-    "signature": signature == null ? null : signature,
+    "offre": offre == null ? null : offre!.toJson(),
     "state": state == null ? null : state,
+    "piecesjointes": piecesjointes == null ? null : piecesjointes!.toJson(),
+    "remote_id": remoteId == null ? null : remoteId,
+  };
+}
+
+class Agent {
+  Agent({
+    this.id,
+    this.identity,
+  });
+
+  int ? id;
+  String? identity;
+
+  factory Agent.fromJson(Map<String, dynamic> json) => Agent(
+    id: json["id"] == null ? null : json["id"],
+    identity: json["identity"] == null ? null : json["identity"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "identity": identity == null ? null : identity,
+  };
+}
+
+class Commune {
+  Commune({
+    this.id,
+    this.name,
+    this.zone,
+  });
+
+  int? id;
+  String? name;
+  Zone? zone;
+
+  factory Commune.fromJson(Map<String, dynamic> json) => Commune(
+    id: json["id"] == null ? null : json["id"],
+    name: json["name"] == null ? null : json["name"],
+    zone: json["zone"] == null ? null : Zone.fromJson(json["zone"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "name": name == null ? null : name,
+    "zone": zone == null ? null : zone!.toJson(),
+  };
+}
+
+class Zone {
+  Zone({
+    this.id,
+    this.name,
+    this.ville,
+  });
+
+  int? id;
+  String? name;
+  Ville? ville;
+
+  factory Zone.fromJson(Map<String, dynamic> json) => Zone(
+    id: json["id"] == null ? null : json["id"],
+    name: json["name"] == null ? null : json["name"],
+    ville: json["ville"] == null ? null : Ville.fromJson(json["ville"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "name": name == null ? null : name,
+    "ville": ville == null ? null : ville!.toJson(),
+  };
+}
+
+class Ville {
+  Ville({
+    this.id,
+    this.name,
+    this.province,
+  });
+
+  int? id;
+  String? name;
+  Province? province;
+
+  factory Ville.fromJson(Map<String, dynamic> json) => Ville(
+    id: json["id"] == null ? null : json["id"],
+    name: json["name"] == null ? null : json["name"],
+    province: json["province"] == null ? null : Province.fromJson(json["province"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "name": name == null ? null : name,
+    "province": province == null ? null : province!.toJson(),
+  };
+}
+
+class Province {
+  Province({
+    this.id,
+    this.name,
+  });
+
+  int? id;
+  String? name;
+
+  factory Province.fromJson(Map<String, dynamic> json) => Province(
+    id: json["id"] == null ? null : json["id"],
+    name: json["name"] == null ? null : json["name"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "name": name == null ? null : name,
+  };
+}
+
+class CompanyType {
+  CompanyType({
+    this.id,
+    this.name,
+  });
+
+  int? id;
+  dynamic name;
+
+  factory CompanyType.fromJson(Map<String, dynamic> json) => CompanyType(
+    id: json["id"] == null ? null : json["id"],
+    name: json["name"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "name": name,
+  };
+}
+
+class Offre {
+  Offre({
+    this.id,
+    this.name,
+  });
+
+  int?id;
+  String? name;
+
+  factory Offre.fromJson(Map<String, dynamic> json) => Offre(
+    id: json["id"] == null ? null : json["id"],
+    name: json["name"] == null ? null : json["name"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "name": name == null ? null : name,
+  };
+}
+
+class Piecesjointes {
+  Piecesjointes({
+    this.id,
+    this.path,
+    this.type,
+  });
+
+  int? id;
+  String? path;
+  String? type;
+
+  factory Piecesjointes.fromJson(Map<String, dynamic> json) => Piecesjointes(
+    id: json["id"] == null ? null : json["id"],
+    path: json["path"] == null ? null : json["path"],
+    type: json["type"] == null ? null : json["type"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "path": path == null ? null : path,
+    "type": type == null ? null : type,
   };
 }
