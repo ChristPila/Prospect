226acfd12313120413c5fc3573f9a31e95deafab
victorine Kaleko <victorinekaleko@gmail.com>
2022-09-22 15:38:09 +0100

    utils utlises pour un prospect

--- lib/utils/utilitaires.dart
+++ lib/utils/utilitaires.dart
@@ -8,7 +8,7 @@ class Utilitaires {
   // faire ipconfig (ifconfig sur Mac) pour recuperer votre IP de la machine
   // et mettre à jour
   static const String HOST_URL = "10.224.196.156";
-  static const String STOCKAGE_VERSION ="V5";
+  static const String STOCKAGE_VERSION ="V12";
 
   static String formatAmount(double valeurNumerique) {
     // cette methode peut etre etre remplacée par le package Intl
