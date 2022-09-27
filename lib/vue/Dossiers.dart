7ddf9a71451d9c6eed9b1e56792dd06fc4cf30f7
victorine Kaleko <victorinekaleko@gmail.com>
2022-09-27 17:14:27 +0100

    la vue des dossiers prospect

--- lib/vue/Dossiers.dart
+++ lib/vue/Dossiers.dart
@@ -1,27 +1,156 @@
-import 'dart:io';
-import 'package:flutter/cupertino.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:prospect/vue/ProspectDetail.dart';
-import '../utils/utilitaires.dart';
+import '../model/ProspectModel.dart';
 
-class Carte extends StatefulWidget {
-  const  Carte({Key? key, required this.title}): super(key: key);
-  final String title;
-
-
+class Dossiers extends StatefulWidget {
+  Dossiers({required this.clientrecup});
+  ProspectModel clientrecup;
   @override
-  State<Carte> createState() => _CarteState();
+  State<Dossiers> createState() => _DossiersState(clientrecup: clientrecup );
 }
 
-class _CarteState extends State<Carte> {
+class _DossiersState extends State<Dossiers> {
+
+  ProspectModel clientrecup;
+  _DossiersState({ required this.clientrecup});
+
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text("Afficher la carte"),
-      centerTitle: true,
-      backgroundColor: Utilitaires.DEFAULT_COLOR,
-    ),
-  );
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Dossiers du prospect"),
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          bottom: TabBar(
+            tabs: [
+              Tab(child: Text("Images", style: TextStyle(color: Colors.black),),
+                icon: Icon(Icons.browse_gallery_outlined, color: Colors.black),
+                
+              ),
+              Tab(
+                child: Text("Documents", style: TextStyle(color: Colors.black),),
+                icon: Icon(Icons.document_scanner, color: Colors.black,),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [tab1(widget.clientrecup), tab2(widget.clientrecup)],
+        ),
+      ),
+    );
+  }
+
+  tab1(clientrecup) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(child: listImagesVue(context,clientrecup)),
+        ],
+      ),
+    );
+  }
+  tab2(clientrecup) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(child: listDocumentsVue(context,clientrecup)),
+        ],
+      ),
+    );
+  }
+  listImagesVue(BuildContext context,clientrecup) {
+    return RefreshIndicator(
+      onRefresh: () async {
+        // effectuer une action asynchrone
+        await Future.delayed(
+            Duration(milliseconds: 800)); // simuler une action asynchrone
+
+        setState(() {});
+      },
+      child:GridView.builder(
+        itemCount: clientrecup.piecesjointes.length,
+        itemBuilder: (BuildContext context, int index) {
+          var img = clientrecup.piecesjointes[index].path;
+          return CachedNetworkImage(
+            imageUrl: "$img",width: 100, height: 50,
+            progressIndicatorBuilder: (context, url, downloadProgress) =>
+                CircularProgressIndicator(value: downloadProgress.progress),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          );
+        },
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, mainAxisSpacing: 10, crossAxisSpacing: 10),
+        padding: const EdgeInsets.all(10),
+        shrinkWrap: true,
+      ),
+    );
+  }
+
+  listDocumentsVue(BuildContext context,clientrecup) {
+    return RefreshIndicator(
+      onRefresh: () async {
+        // effectuer une action asynchrone
+        await Future.delayed(
+            Duration(milliseconds: 800)); // simuler une action asynchrone
+
+        setState(() {});
+      },
+      child: ListView.builder(
+        itemCount: clientrecup.piecesjointes.length,
+        itemBuilder: (BuildContext context, int index) {
+          var doc =clientrecup.piecesjointes[index];
+          var completePath = doc.path;
+          var fileName = (completePath.split('./').last);
+          var fileExtension = (fileName.split("/").last);
+          return ListTile(
+            onTap: () {
+              final scaffold = ScaffoldMessenger.of(context);
+              scaffold.showSnackBar(SnackBar(
+                content: Text("J'ai cliqué sur $doc"),
+                action: SnackBarAction(
+                    label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
+              ));
+            },
+            title: Row(
+              children: [
+                  Container(
+                    child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              fileExtension,
+                              style: TextStyle(fontSize: 25, color: Colors.red),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              fileName,
+                              style:
+                              TextStyle(fontSize: 10, color: Colors.black),
+                            ),
+                          ],
+                        )),
+                  ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
