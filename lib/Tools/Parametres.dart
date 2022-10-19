2dcd879b0dc680d16270f47fc660cf665f9b1f44
McNorth <northonmambo19@gmail.com>
2022-10-19 18:35:05 +0100

    ajout du endpoits

--- lib/Tools/Parametres.dart
+++ lib/Tools/Parametres.dart
@@ -4,5 +4,9 @@ class Parametres{
   static const String host = "127.0.0.1";
   static const int port = 8000;
   static const String endPointLogin = "/api/login";
-  static const String ROOT = "admin@admin.com:123456";
+  static const String endSevenLastDays = "/api/lastSevenDays";
+  static const String endThreeLastDays = "/api/lastThreeDays";
+  static const String endThreeLastMonths = "/api/lastThreeMonths";
+  static const String endThreeLastWeeks = "/api/lastThreeWeeks";
+  static const String endDayToDate = "/api/dayToDate";
 }
