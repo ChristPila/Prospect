4b5664ccf05b8522d0ef218b89c001d984471c3c
McNorth <northonmambo19@gmail.com>
2022-10-20 16:27:40 +0100

    reformulation des paths performance utilisateur

--- lib/Tools/Parametres.dart
+++ lib/Tools/Parametres.dart
@@ -4,11 +4,11 @@ class Parametres{
   static const String host = "127.0.0.1";
   static const int port = 8000;
   static const String endPointLogin = "/api/login";
-  static const String endSevenLastDays = "/api/lastSevenDays";
-  static const String endThreeLastDays = "/api/lastThreeDays";
-  static const String endThreeLastMonths = "/api/lastThreeMonths";
-  static const String endThreeLastWeeks = "/api/lastThreeWeeks";
-  static const String endDayToDate = "/api/dayToDate";
+  static const String endSevenLastDays = "/api/reporting/last_seven_days";
+  static const String endThreeLastDays = "/api/reporting/last_three_days";
+  static const String endThreeLastMonths = "/api/reporting/last_three_months";
+  static const String endThreeLastWeeks = "/api/reporting/last_three_weeks";
+  static const String endDayToDate = "/api/reporting/day_to_date";
   static const String endPointOffres = 'api/offres';
   static const String endPointProvinces = 'api/provinces';
   static const String endPointZones = '';
