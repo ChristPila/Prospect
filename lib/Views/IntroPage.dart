b3c2c8079144df80c56430f4fbbea0f056f7185e
obidano <omotetedan2@gmail.com>
2022-11-30 11:52:34 +0100

    verification session pour redirection route

--- lib/Views/IntroPage.dart
+++ lib/Views/IntroPage.dart
@@ -1,10 +1,37 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:provider/provider.dart';
 
+import '../Controllers/AuthentifacationController.dart';
 import 'AuthentificationPage.dart';
+import 'HomePage.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  }
+
+  verifierSiDejaConnecte(){
+    context.read<AuthentificationController>().session();
+    var session = context.read<AuthentificationController>().utilisateur;
+    if (session != null) {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) {
+        return HomePage();
+      }));
+      return null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +40,8 @@ class IntroPage extends StatelessWidget {
       backgroundColor: Colors.white,
       splashIconSize: 150,
       screenFunction: () async {
+        var homeRoute=verifierSiDejaConnecte();
+        if(homeRoute!=null) return homeRoute;
         return AuthentificationPage();
       },
       splashTransition: SplashTransition.slideTransition,
